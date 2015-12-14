748aade03ec83fcb2ef82aea2b08acdae53441c5
Tore Eschliman <toregeschliman@gmail.com>
2015-12-14 00:56:00 -0600

    added README, fleshed out a bit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Steve's Web scraper
+
+This is a simple Selenium Webdriver API endpoint for scraping business data from websites. Pass it a json dict formatted like sample.json, with xpaths for all your elements, and it will return a similar JSON dict with the text content. sample-frontend shows you how to submit requests and write the results as a csv.
--- web-scraper.py
+++ web-scraper.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as # coding=utf-8
+from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
 
 class WebScraper():
@@ -13,6 +13,7 @@ class WebScraper():
         entries = WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located(('xpath', self.params['key_xpath'])))
         for entry in entries:
             result = {}
-            for key, value in 
-
+            for key, value in self.params['business_data']:
+                result[key] = self.driver.get_element_by_xpath(value).text
+                results.append(result)
         return results
