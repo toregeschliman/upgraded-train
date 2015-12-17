e270230f0e73b2b346347bc8d05776f19c98150f
Tore Eschliman <toregeschliman@gmail.com>
2015-12-17 09:54:45 -0600

    errors

--- web_scraper.py
+++ web_scraper.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class WebScraper():
     def __init__(self, json_dict):
